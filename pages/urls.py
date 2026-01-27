caee985017999d1a3a6139c9d48d3510ee9ccc5b
oLagadoo <fvcklagadoo@gmail.com>
2026-01-27 14:08:31 -0300

    Adiciona rotas e aplica herança de templates

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("sobre/", views.sobre, name="sobre"),
+    path("contato/", views.contato, name="contato"),
+    path("ajuda/", views.ajuda, name="ajuda"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     contexto = {
-        "mensagem": "Servidor ligado e respondendo."
+        "mensagem": "Servidor ligado e respondendo. Parabéns, você colocou um back-end no ar."
     }
     return render(request, "pages/home.html", contexto)
+
+def sobre(request):
+    return render(request, "pages/sobre.html")
+
+def contato(request):
+    return render(request, "pages/contato.html")
+
+def ajuda(request):
+    return render(request, "pages/ajuda.html")
